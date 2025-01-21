903ab93eecc9c52dc820c73a9c79632bb79f845d
JW_He <61524560+Miemieku@users.noreply.github.com>
2025-01-21 14:55:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 1️⃣ 创建地图，中心点设为 Darmstadt
+    // 1️⃣ 创建地图，中心点设为 Düsseldorf
     var map = L.map('map').setView([49.8728, 8.6512], 12);
 
     // 2️⃣ 添加 OpenStreetMap 瓦片图层
