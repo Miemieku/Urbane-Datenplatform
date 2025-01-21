f59b1ddcf614ece29c7354db0afa0c7b3b7f7621
JW_He <61524560+Miemieku@users.noreply.github.com>
2025-01-21 14:09:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var map = L.map('map').setView([49.8728, 8.6512], 12); // 设置初始中心（达姆施塔特）
+
+    // 添加 OpenStreetMap 瓦片图层
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+});
 
